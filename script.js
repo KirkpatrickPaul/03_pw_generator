cd8c878faa07e29901b434fa17197f8b5ad8b538
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-09-29 20:33:58 -0500

    finished prompts

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var characters = []
-function generatePassword(){
-  // Found array of uppercase and lowercase characters at https://gist.github.com/bendc/1e6af8f2d8027f2965da also, I decided to only include common and easy to read password special characters and not, for instance, math special characters or special characters only accessible by alt code.
-  characters = {capital:
+var characters = {capital:
   ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], lowercase:
   ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"], number:
   ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"], symbol:
   ["~", "!", "@", "#", "$", "%", "&", "?"]
-  }
-  var passwordLength = prompt("How long do you want your password to be?", "8 to 128 characters please")
-  // passwordLength = Number(passwordLength)
+  };
+
+function generatePassword(){
+  // Found array of uppercase and lowercase characters at https://gist.github.com/bendc/1e6af8f2d8027f2965da also, I decided to only include common and easy to read password special characters and not, for instance, math special characters or special characters only accessible by alt code.
+  var passwordLength = prompt("How long do you want your password to be?", "8 to 128 characters please");
   while(isNaN(passwordLength*0)){
-    passwordLength = prompt(" Please only input numbers!", "Password Length")
-    passwordLength = Number(passwordLength)
+    passwordLength = prompt(" Please only input numbers!", "Password Length");
+    passwordLength = Number(passwordLength);
     while(passwordLength < 8 || passwordLength > 128){
-      passwordLength = prompt ("Only numbers from 8 to 128 please!", "Password Length")
-      passwordLength = Number(passwordLength)
+      passwordLength = prompt ("Only numbers from 8 to 128 please!", "Password Length");
+      passwordLength = Number(passwordLength);
     }
     }
-    console.log(typeof passwordLength)
+    var useCaps = confirm('Would you like to use capital letters in your password? Press "OK" for yes, and "Cancel" for no.')
+    var useLowercase = confirm('Would you like to use lowercase letters in your password? Press "OK" for yes, and "Cancel" for no.')
+    var useNumbers = confirm('Would you like to use numbers in your password? Press "OK" for yes, and "Cancel" for no.')
+    var useSymbols = confirm('Would you like to use symbols in your password? Press "OK" for yes, and "Cancel" for no.')
+    
   }
 // Write password to the #password input
 function writePassword() {
