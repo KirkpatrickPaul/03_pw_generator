dfcd58a05de024b99b1b743233f677da2233a58b
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-09-30 21:30:39 -0500

    code works as intended. Not using any character types ends with no password.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-  // Found array of uppercase and lowercase characters at https://gist.github.com/bendc/1e6af8f2d8027f2965da also, I decided to only include common and easy to read password special characters and not, for instance, math special characters or special characters only accessible by alt code.
+  // Found array of uppercase and lowercase characters at https://gist.github.com/bendc/1e6af8f2d8027f2965da
+  //I decided to only include common and easy to read password special characters and not, for instance, math special characters or special characters only accessible by alt code.
   
   var characters = {capital: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
     lowercase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
@@ -13,20 +14,19 @@ function generatePassword(){
   var passwordLength = prompt("How long do you want your password to be?", "8 to 128 characters please");
   while(isNaN(passwordLength*0)){
     passwordLength = prompt(" Please only input numbers!", "Password Length");
-    passwordLength = Number(passwordLength);
     while(passwordLength < 8 || passwordLength > 128){
       passwordLength = prompt ("Only numbers from 8 to 128 please!", "Password Length");
-      passwordLength = Number(passwordLength);
+      };
     };
-    };
+    passwordLength=Number(passwordLength)
   var useCaps = confirm('Would you like to use capital letters in your password? Press "OK" for yes, and "Cancel" for no.');
   var useLowercase = confirm('Would you like to use lowercase letters in your password? Press "OK" for yes, and "Cancel" for no.');
   var useNumbers = confirm('Would you like to use numbers in your password? Press "OK" for yes, and "Cancel" for no.');
   var useSymbols = confirm('Would you like to use symbols in your password? Press "OK" for yes, and "Cancel" for no.');
   if (useCaps+useLowercase+useNumbers+useSymbols === 0) {
-    var charactersWarning = alert("You must use at least one type of characters!");
-    return
-  }
+    alert("You must use at least one type of character!");
+    return "No Password"
+  };
   if (useCaps === false) {
     delete characters.capital
   };
@@ -39,12 +39,22 @@ function generatePassword(){
   if (useSymbols === false) {
     delete characters.symbol
   };
-  // building the password
-  for (var i=0; i<(length.passwordLength); i++) {
-    var rngType = Math.floor(Math.random() * length.characters);
-    var rngSpot = Math.floor(Math.random() * length.characters[rngType]);
-    theActualPassword = theActualPassword.push(characters[rngType][rngSpot])
-  }
+  // building the password. The length-charKeys.length is so that we can later insert a random character of each type required to ensure at least one of each.
+  var charKeys = Object.keys(characters)
+  for (var i=0; i<(passwordLength - charKeys.length); i++) {
+    var rngType = charKeys[Math.floor(Math.random() * charKeys.length)];
+    var rngSpot = Math.floor(Math.random() * characters[rngType].length);
+    theActualPassword = theActualPassword + characters[rngType][rngSpot];
+  };
+  //generate a random character from each character type, then randomly insert it into the string. 
+  //Found how to modify middle of string from https://stackoverflow.com/questions/4313841/insert-a-string-at-a-specific-index
+  for (var i=0; i<charKeys.length; i++) {
+    var newCharacter = characters[(charKeys[i])][Math.floor(Math.random() * characters[(charKeys[i])].length)];
+    var newPlacement = Math.floor(Math.random() * theActualPassword.length);
+    newPlacement = Number(newPlacement);
+    theActualPassword = theActualPassword.substr(0, newPlacement) + newCharacter + theActualPassword.substr(newPlacement);
+  };
+  return theActualPassword
   }
 // Write password to the #password input
 function writePassword() {
